1c7b6fac8ee0d5fa625b3a9f2da7ab966727440f
Berkay ÇINAR <143068984+Software-1923@users.noreply.github.com>
2024-05-11 12:13:55 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,31 +1,48 @@
-// data.js
-const Web3 = require('web3');
-const web3 = new Web3('https://eth-mainnet.g.alchemy.com/v2/W0euO4lKSsktbMBH3QVnxMR2Vkbh5qIm');
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+const detectEthereumProvider = require('@metamask/detect-provider');
+const dotenv = require('dotenv').config({ path: './.env' }); // dotenv modülünü kullanarak server.env dosyasını oku
+
+const alchemyApiKey = process.env.ALCHEMY_API_KEY;
+const alchemyUrl = process.env.ALCHEMY_URL; // Yeni eklenen Alchemy URL parametresi
+
+const alchemyWeb3 = createAlchemyWeb3(alchemyApiKey, alchemyUrl); // Alchemy URL parametresini createAlchemyWeb3 fonksiyonuna ekleyin
 
 async function fetchData() {
   try {
-    const accounts = await web3.eth.getAccounts();
-    console.log('Connected accounts:', accounts);
-    return accounts;
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      if (provider !== window.ethereum) {
+        console.error('Doğru Ethereum sağlayıcısı bulunamadı.');
+        return;
+      }
+      console.log('Metamask sağlayıcısı algılandı.');
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      console.log('Connected accounts:', accounts);
+      return accounts;
+    } else {
+      console.error('Metamask sağlayıcısı bulunamadı.');
+      return;
+    }
   } catch (error) {
-    console.error('Web3 interaction error:', error.message);
-    throw new Error('Error during Web 3 interaction');
+    console.error('Erişim engellendi:', error.message);
+    throw new Error('Error during Metamask interaction');
   }
 }
 
-// fetchData functionunu çağıran bir async fonksiyon içinde çalıştırılmalıdır
 async function main() {
   try {
     const accounts = await fetchData();
-    const hardhatConfig = require('./hardhat.config.js'); // Dosya yolunu doğru bir şekilde güncelleyin
-    console.log('Hardhat config result:', await hardhatConfig(accounts));
+    // Burada hardhat.config.js dosyasıyla ilgili bir işlem yapılıyor. Bu kısmı projenize göre güncelleyin.
+    // Örneğin, hardhat.config.js dosyasını require ederek veya fonksiyon çağrısı yaparak kullanabilirsiniz.
+    // const hardhatConfig = require('./hardhat.config.js');
+    // console.log('Hardhat config result:', await hardhatConfig(accounts, ethAddress));
   } catch (error) {
-    console.error('Error during main function execution:', error.message);
+    console.error('Ana fonksiyonun çalıştırılması sırasında hata oluştu:', error.message);
   }
 }
 
 main();
 
 module.exports = fetchData;
-
 
