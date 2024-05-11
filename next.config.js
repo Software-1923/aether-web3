c04c85018a44e952a1fb732ec9e242611d5df239
Berkay ÇINAR <143068984+Software-1923@users.noreply.github.com>
2024-05-11 17:38:28 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
-  // Specify the directory where Next.js should look for pages
-  // You can adjust this path according to your project structure
-  pagesDir: './dist/',
+  // Specify the custom build directory
+  distDir: 'dist',
+  // Other configurations...
 };
