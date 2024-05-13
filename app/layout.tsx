a8d481660961987b5075ed449876fa199503dc92
Berkay ÇINAR <143068984+Software-1923@users.noreply.github.com>
2024-05-13 17:26:15 +0000

    Data Fortress Inc.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from "@vercel/analytics/react"
- 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function RootLayout({
   children,
 }: {
