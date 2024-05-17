dcd6406b125357d192df9603e2d6604ae6f371e5
Berkay ÇINAR <143068984+Software-1923@users.noreply.github.com>
2024-05-17 20:33:15 +0300

    Create index.tsx

---
+++ dist/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="bg-gray-900 text-white flex justify-center items-center min-h-screen">
+      <div className="container text-center">
+        <div className="cool font-bold text-3xl leading-6 max-w-20rem inline-block">
+          <span className="text-shadow">Home</span>
+        </div>
+        <div className="terminal relative">
+          <h1 className="font-mono text-green-500">Page</h1>
+          <div className="before-circle absolute top-0 left-0 h-4 bg-gray-700 text-green-500 w-full text-2xl line-0 p-14 text-indent-4">
+            <div className="mx-auto w-4 h-4 bg-green-500 rounded-full"></div>
+          </div>
+        </div>
+      </div>
+
+      <div id="otherside-root" style={{ zIndex: 2147483647, position: 'relative' }}>
+        <div id="hyper-main-container">
+          <div id="hyper-menu-shadow-root"></div>
+          <div id="assistant-shadow-root"></div>
+        </div>
+      </div>
+
+      <script>
+        {`
+          var currentUrl = window.location.href;
+
+          if (currentUrl.endsWith('.html')) {
+            var newUrl = currentUrl.slice(0, -5);
+            window.history.replaceState({}, document.title, newUrl);
+          }
+        `}
+      </script>
+    </div>
+  );
+};
+
+export default HomePage;
